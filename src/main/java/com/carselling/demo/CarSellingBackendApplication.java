b4e606878153dc043cc6d2f3b58f076167b8bbbf
vladmihalca2020 <mihalca_vlad@yahoo.ro>
2020-12-13 16:31:37 +0200

    I created the project

---
+++ src/main/java/com/carselling/demo/CarSellingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.carselling.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSellingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarSellingBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/carselling/demo/CarSellingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carselling.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSellingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
